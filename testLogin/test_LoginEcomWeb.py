f0bb049cbb03f210637aa799e17271ad15b586f5
natthathidapop <natthapop.k@gmail.com>
2025-02-03 01:32:02 +0700

    Updated test script

--- testLogin/test_LoginEcomWeb.py
+++ testLogin/test_LoginEcomWeb.py
@@ -4,40 +4,49 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time 
 
-# open Chrome Browser
 driver = webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("http://localhost/otop_phrae_shop/home.php")
 time.sleep(3)
 
-# wait for login button
+
+# wait for login button, then click
 login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Login")))
 login_button.click()
 time.sleep(3)
 
-# input username
+# try logging in with the wrong password
 username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "username")))
-username_input.send_keys("nattha")
-
-# input password
 password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password")))
-password_input.send_keys("pop1234")
 
-# Sign In
+username_input.send_keys("nattha")
+password_input.send_keys("123456789")  # wrong password
+
 sign_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='submit' and @value='Sign In']")))
 sign_in_button.click()
 time.sleep(3)
 
+# Try logging in with the correct password.
+username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "username")))
+password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password")))
 
-# Log out after successful login
+username_input.clear()
+password_input.clear()
+time.sleep(2)
+
+username_input.send_keys("nattha")
+password_input.send_keys("pop1234")  # correct password
+
+# wait for sign in button and click
+sign_in_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='submit' and @value='Sign In']")))
+sign_in_button.click()
+time.sleep(5)  # wait for web load
+
+# check login successful (Logout button must show)
 logout_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Logout')]")))
 logout_button.click()
-
-# show result for 3 seconds
 time.sleep(3)
 
-login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Login")))
-time.sleep(3)
-# close
+#close
 driver.quit()
